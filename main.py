b2488af5cbf1db1cd59ae053efdb1cb4cfbca650
Akilesh-programmer <akileshakileshs1234@gmail.com>
2021-12-05 17:12:22 +0530

    Wrote the main code

--- main.py
+++ main.py
@@ -1,56 +1,32 @@
-# Importing the turtle class and screen class from turtle module.
-from turtle import Screen, Turtle
-import snake
-# Importing time module.
+from turtle import Screen
+from snake import Snake
 import time
-# Creating a screen object. This is gonna be the window that shows up.
+
+
 screen = Screen()
-
-# Setting the height and width of the screen.
 screen.setup(height=600, width=600)
-
-# Changing the background color of the screen.
 screen.bgcolor("black")
-
-# Setting a title for the screen.
 screen.title("Snake Game")
 
-# When we are playing the snake game then we could see some animations, they are not needed and they don't look good. For example we will be three squares, they won't come up all at the same time, first the first one will be drawn, next the next one will be drawn, so we could see that animation. We can turn that off with the help of the below method associated with screen. Also we need to say when we want to update the screen, that it to show changes, so that at that line of code only the entire new thing will be printed. So we don't see the animation. We need to give the paramater to zero to turn the animation off.
-screen.tracer(0)
+screen.tracer(0) 
 
-
-
-# Updating the screen.
-screen.update()
-
-sup = snake.Snake()
-
-# Creating a boolean variable to show that game is on.
 game_is_on = True
 
-# Creating a while loop to run the game that is to move the squares.
+snake = Snake()
+
+screen.listen()
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
+
 while game_is_on:
     screen.update()
-    time.sleep(1)
-    sup.forward()
-    sup.forward()
-    screen.update()
-    sup.turn_right()
+    time.sleep(0.09)
 
+    snake.move()
+    
 
+    
 
-
-
-
-
-
-            
-
-
-
-
-
-
-
-# Using a method to close the screen only when we click on it.
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,59 +1,46 @@
 from turtle import Turtle
 
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 class Snake:
     def __init__(self):
-        self.starting_positions = [(0, 0), (-20, 0), (-40, 0)]
         self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
 
-        for position in self.starting_positions:
-            self.new_segment = Turtle(shape="square")
-            self.new_segment.penup()
-            self.new_segment.color("white")
-            self.new_segment.goto(position)
-            self.segments.append(self.new_segment)
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle(shape="square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)       
 
-    def forward(self):
-        for segment in self.segments:
-            segment.forward(20)
+    
+    def move(self):
+        for seg_num in range(len(self.segments) -1, 0, -1):
+            self.new_x = self.segments[seg_num-1].xcor()
+            self.new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(self.new_x, self.new_y)
+        self.head.forward(MOVE_DISTANCE)
 
-    def turn_left(self):
-        number_of_segments = 0
-        for segment in self.segments:
-            number_of_segments += 1
-        
-        times_running = 0
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
-        for segment in self.segments:
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
-            tobe = 0
-            times_running += 1
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
-            if times_running != 1:
-                for n in range(times_running - 1):
-                    segment.forward(20)
-                    tobe += 1
-            segment.left(90)
-
-            for n in range(number_of_segments - tobe):
-                segment.forward(20)
-
-    def turn_right(self):
-        number_of_segments = 0
-        for segment in self.segments:
-            number_of_segments += 1
-        
-        times_running = 0
-
-        for segment in self.segments:
-
-            tobe = 0
-            times_running += 1
-
-            if times_running != 1:
-                for n in range(times_running - 1):
-                    segment.forward(20)
-                    tobe += 1
-            segment.right(90)
-
-            for n in range(number_of_segments - tobe):
-                segment.forward(20)
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
